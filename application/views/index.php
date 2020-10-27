8da60b36c07373dc7e52b4e4ad8bcf8d0b9e6ec7
Hector Matias Reyes Albornoz <hector@MacBook-Air-de-Hector.local>
2020-10-26 23:17:59 -0300

    cambio index

--- application/views/index.php
+++ application/views/index.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
 	<meta charset="utf-8">
-	<title>Ingreso Usuario 2</title>
+	<title>Ingreso Usuario 22222</title>
 	<link rel="stylesheet" href="/clases/css/estilos.css">
 	
 </head>
